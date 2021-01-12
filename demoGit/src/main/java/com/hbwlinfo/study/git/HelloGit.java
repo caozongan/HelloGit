c1e35d88eb72d77f6d8896994d04674b8ea0e51c
caozongan <caozongan@qq.com>
2021-01-12 15:03:20 +0800

    2021.01.12添加建立分支

--- demoGit/src/main/java/com/hbwlinfo/study/git/HelloGit.java
+++ demoGit/src/main/java/com/hbwlinfo/study/git/HelloGit.java
@@ -7,7 +7,9 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
-
+        System.out.println("更新2");
+        System.out.println("更新3");
+        System.out.println("建立分支");
 
 
 
