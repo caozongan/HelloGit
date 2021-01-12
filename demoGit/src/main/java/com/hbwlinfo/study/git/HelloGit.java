38176a80907291d0a22f8c89f2a21119d9a09e22
caozongan <caozongan@qq.com>
2021-01-12 14:53:27 +0800

    2021.01.12添加1

--- demoGit/src/main/java/com/hbwlinfo/study/git/HelloGit.java
+++ demoGit/src/main/java/com/hbwlinfo/study/git/HelloGit.java
@@ -6,7 +6,8 @@ package com.hbwlinfo.study.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Delete!");
+        System.out.println("更新1");
+
 
 
 
